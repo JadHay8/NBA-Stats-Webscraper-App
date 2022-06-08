bc53f93b5a329a2fc568858016b464e27be2d11c
Jad Haytaoglu <jadhaytaoglu@hotmail.com>
2022-06-08 16:35:07 -0400

    Removed local json database and reorganized data into a single stats array with stat-corresponding sorting functions

--- tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-            // if (points.attr('data-stat') === 'pts_per_g'){
-            //     points = points.text()
-            //     // articles.push({
-            //     //     points
-            //     // })
-            // }
